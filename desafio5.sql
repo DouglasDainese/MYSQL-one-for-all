a831da2c0b7679b82287c935cb4eac69a205d0e1
DouglasDainese <douglas_dainese@hotmail.com>
2023-01-27 15:34:12 -0300

    fix: remoção de INNER JOIN desnecessário

--- desafio5.sql
+++ desafio5.sql
@@ -1,18 +1,13 @@
 SELECT 
 	S.songs_title "cancao",
-    COUNT(U.user_id) "reproducoes"
+    COUNT(S.songs_id) "reproducoes"
 FROM 
 	SpotifyClone.songs S
 INNER JOIN
 	SpotifyClone.history H
 ON
 	H.music_playback_history = S.songs_id
-INNER JOIN
-	SpotifyClone.users U
-ON 
-	U.user_id = H.user_id
 GROUP BY 
 	cancao
 ORDER BY
 	reproducoes DESC, cancao LIMIT 2;
-  
